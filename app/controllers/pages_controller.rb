7e2be2a8d813887058174892167c4db4835972a5
dev-stan <mrstanleyzalewski@gmail.com>
2024-11-10 17:25:59 +0100

    Update cards

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,40 +1,40 @@
 # app/controllers/pages_controller.rb
 class PagesController < ApplicationController
-  protect_from_forgery with: :null_session  # Necessary for API-like endpoints
-
-  def home
-    @mapbox_token = ENV["MAPBOX_TOKEN"]
-    @satelites_list = SateliteTrack.get_satelite_nearby(52.409538, 16.931992, altitude=0, radius=60)
-
-  end
-
-  def search
-    city = params[:query]
-    if city.blank?
-      render json: { error: "City name cannot be blank." }, status: :bad_request
-      return
+    protect_from_forgery with: :null_session  # Necessary for API-like endpoints
+  
+    def home
+      @mapbox_token = ENV["MAPBOX_TOKEN"]
+      @satelites_list = SateliteTrack.get_satelite_nearby(52.409538, 16.931992, altitude=0, radius=60)
+  
     end
-
-    coordinates = SateliteTrack.geocode_city(city)
-    if coordinates.nil?
-      render json: { error: "Could not find the city: #{city}" }, status: :not_found
-      return
+  
+    def search
+      city = params[:query]
+      if city.blank?
+        render json: { error: "City name cannot be blank." }, status: :bad_request
+        return
+      end
+  
+      coordinates = SateliteTrack.geocode_city(city)
+      if coordinates.nil?
+        render json: { error: "Could not find the city: #{city}" }, status: :not_found
+        return
+      end
+  
+      satelite_list = SateliteTrack.get_satelite_nearby(
+        coordinates[:latitude],
+        coordinates[:longitude],
+        altitude = 0,
+        radius = 30
+      )
+  
+      render json: {
+        satellites: satelite_list,
+        observer_lat: coordinates[:latitude],
+        observer_lng: coordinates[:longitude]
+      }
+    rescue StandardError => e
+      Rails.logger.error("Search Error: #{e.message}")
+      render json: { error: "An error occurred while processing your request." }, status: :internal_server_error
     end
-
-    satelite_list = SateliteTrack.get_satelite_nearby(
-      coordinates[:latitude],
-      coordinates[:longitude],
-      altitude = 0,
-      radius = 30
-    )
-
-    render json: {
-      satellites: satelite_list,
-      observer_lat: coordinates[:latitude],
-      observer_lng: coordinates[:longitude]
-    }
-  rescue StandardError => e
-    Rails.logger.error("Search Error: #{e.message}")
-    render json: { error: "An error occurred while processing your request." }, status: :internal_server_error
   end
-end
--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,6 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin "popper", to: "popper.js", preload: true
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
+# config/importmap.rb
+pin "mapbox-gl", to: "https://unpkg.com/mapbox-gl@2.15.0/dist/mapbox-gl.js", preload: true
+
