31a57f0ccf2d3952230508cb99138d25edbb1029
dev-stan <mrstanleyzalewski@gmail.com>
2024-11-10 01:00:22 +0100

    Syntax fix #3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  #STAN NOTE the code below made it impossible to view website from mobile
-  #allow_browser versions: :modern
+  # STAN NOTE: The code below made it impossible to view the website from mobile
+  # allow_browser versions: :modern
 
-  # code below helps out with meta tags 
+  # Code below helps out with meta tags 
   def default_url_options
-    { host: ENV["DOMAIN”] || """localhost:3000"}
-    end
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
 end
