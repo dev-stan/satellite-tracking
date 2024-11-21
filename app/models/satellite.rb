5d3d5c99c8edacc83020a65573572b23e2b4258f
dev-stan <mrstanleyzalewski@gmail.com>
2024-11-22 00:33:14 +0100

    Add satellite associations

--- app/models/satellite.rb
+++ app/models/satellite.rb
@@ -1,2 +1,5 @@
 class Satellite < ApplicationRecord
+    has_many :user_satellites
+    has_many :saved_by_user, through: :user_satellites, source: :user
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :user_satellites, dependend: :destroy
+  has_many :saved_satellites, through: :user_satellites, source: :satellite
+
   validates :email, presence: true
   validates :password, presence: true, length: { in: 6..30}
   validates :password_confirmation, presence: true
