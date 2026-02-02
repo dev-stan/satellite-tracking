4c53d35ecfb875a279f0b96aff60a8ab144b4b9b
dev-stan <stachuzalewski9@gmail.com>
2026-02-02 18:08:34 +0100

    Fix: JS import on production, wrong path

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,7 +4,6 @@ pin "application"
 
 pin "popper", to: "popper.js", preload: true
 pin "bootstrap", to: "bootstrap.min.js", preload: true
-# config/importmap.rb
 
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
@@ -15,5 +14,3 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 
 # Pin the controllers index file
 pin "controllers", to: "controllers/index.js"
-pin "@hotwired/stimulus", to: "stimulus.min.js"
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
